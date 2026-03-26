7a395301a3e1cf8097ed5a87f9ee2aed49fab7fa
gayatrik27 <gayatri.k2875@gmail.com>
2026-03-27 02:59:09 +0530

    fixed backend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const API_KEY = "ed112898";
+
+// Root route (important for Render test)
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 
 app.get("/movies", async (req, res) => {
   const query = req.query.q;
 
   try {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+    );
     const data = await response.json();
 
     res.json(data);
@@ -22,5 +30,5 @@ app.get("/movies", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
